1fe18e4755ae076d6e02a01ba897b84e1cbf795d
agunning <9905257g@student.gla.ac.uk>
2022-03-30 21:33:54 +0100

    chapter 10 tests passing

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from unicodedata import category
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.forms import CategoryForm
@@ -12,6 +14,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days >0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
+
 # Create your views here.
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -20,10 +41,20 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict)
+    
+    visitor_cookie_handler(request)
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+ 
+    return response
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = int(request.COOKIES.get('visits',1))
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
